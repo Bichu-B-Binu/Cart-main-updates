e87856d31c81516fb8f8e5c4957453d3937a37bf
Bichu-B-Binu <bichubbinu77@gmail.com>
2024-07-10 15:00:25 +0530

    created auth and user api slice

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,16 @@
-import {configureStore} from "@reduxjs/toolkit"
-import { apiSlice } from "./slice/apiSlice"
-import cartSliceReducer from "./slice/cartSlice"
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./slice/apiSlice";
+import cartSliceReducer from "./slice/cartSlice";
+import authSlice from "./slice/authSlice";
 
-const store =configureStore({
-    reducer:{
-        [apiSlice.reducerPath]:apiSlice.reducer,
-        cart:cartSliceReducer,
-    },
-    middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(apiSlice.middleware),
-    devTools:true,
-})
-export default store
+const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    cart: cartSliceReducer,
+    auth: authSlice,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
+});
+export default store;
