88b62f7685fe23b6302a633b46e6b70046e74ed0
Bichu-B-Binu <bichubbinu77@gmail.com>
2024-07-10 16:07:42 +0530

    login screen created and functionnality added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { HomeScreens } from "./components/screens/HomeScreens";
 import { Route, Routes } from "react-router-dom";
 import { ProductScreen } from "./components/screens/ProductScreen";
 import CartScreen from "./components/screens/CartScreen";
+import LoginScreen from "./components/screens/LoginScreen";
 function App() {
   return (
     <>
@@ -13,7 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<HomeScreens />} />
           <Route path="product/:id" element={<ProductScreen />} />
-          <Route path="/cart" element={<CartScreen/>} />
+          <Route path="/cart" element={<CartScreen />} />
+          <Route path="/login" element={<LoginScreen />} />
         </Routes>
       </main>
       <Footer />
